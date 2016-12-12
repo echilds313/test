e09042e5cc5258f141aa87ccc4120a308f2d21de
Eric Childs <echilds313@yahoo.com>
2016-12-12 03:55:11 -0500

    updated

--- phpFeed.php
+++ phpFeed.php
@@ -2,25 +2,43 @@
 //retrieves Atom feeds from PHP.net using simple_xml
 function ReadFeeds($limit)
 {
-$feed_url = "http://php.net/feed.atom";
-//simplexml function creates object
-$feed = simplexml_load_file($feed_url);
+    $feed_url = "http://php.net/feed.atom";
+    //simplexml function creates object
+    $feed = simplexml_load_file($feed_url);
 
-$x = 1;
-foreach($feed->entry as $item)
-{
-//set the limit of feed posts you want to retrieve
-if ($x <= $limit)
-{
-$title = $item->title;
-$url = $item->id;
-echo "<ul id=\"feed\" style='position:relative !important; border:none; display:block;'>
-<li><a href=\"$url\">$title</a></li>
-</ul>";	
+    $x = 1;
+    $output = '<ul class="feed">';
+    foreach ($feed->entry as $item) {
+        //set the limit of feed posts you want to retrieve
+        if ($x <= $limit) {
+            $title = $item->title;
+            $url = $item->id;
+            $output .= "<li><a href='{$url}'>{$title}</a></li>";
+        }
+        $x++;
+    }
+    $output .= '</ul>';
+    echo $output;
 }
-$x++;
-}
-}
-//set number here 
-echo ReadFeeds(3);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Feeds</title>
+  <style>
+  .feed {
+    position:relative !important;
+    border:none;
+    display:block;
+  }
+  </style>
+</head>
+<body>
+  <section>
+    <?php
+       ReadFeeds(4);
+    ?>
+  </section>
+</body>
+</html>
